1402f643b271649edfdfa13abf51a4bd21f97ad5
Ligiano Azzi <ldaazzi@gmail.com>
2020-05-15 16:10:31 +0100

    configuring Post mass assignment

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable = ['title', 'body'];
+    // $guarded is the inverse of fillable
+    // can be used only one at a time
+    // protected $guarded = ['id', 'created_at', 'updated_at'];    
 }
